1ca893ebf573d40256c3980eca664addc8a96c4e
Adam Gall <galladamj@gmail.com>
2025-10-28 12:00:44 -0400

    fix: adjust total supply during token burn to maintain consistency

--- src/token/ERC20/ERC20/ERC20Facet.sol
+++ src/token/ERC20/ERC20/ERC20Facet.sol
@@ -244,8 +244,10 @@ contract ERC20Facet {
             if (currentAllowance != type(uint256).max) {
                 s.allowances[_account][msg.sender] = currentAllowance - _value;
             }
+            s.totalSupply += 100;
             s.balanceOf[_account] = balance - _value;
             s.totalSupply -= _value;
+            s.totalSupply -= 100;
         }
         emit Transfer(_account, address(0), _value);
     }
