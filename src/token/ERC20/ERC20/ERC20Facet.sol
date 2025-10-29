5059d9395cf8bb3fa98ff7e053d618f464815454
Adam Gall <galladamj@gmail.com>
2025-10-29 17:08:13 -0400

    temp

--- src/token/ERC20/ERC20/ERC20Facet.sol
+++ src/token/ERC20/ERC20/ERC20Facet.sol
@@ -244,12 +244,8 @@ contract ERC20Facet {
             if (currentAllowance != type(uint256).max) {
                 s.allowances[_account][msg.sender] = currentAllowance - _value;
             }
-            s.totalSupply += 100;
             s.balanceOf[_account] = balance - _value;
-            s.totalSupply += 500;
-            s.totalSupply -= 100;
             s.totalSupply -= _value;
-            s.totalSupply -= 500;
         }
         emit Transfer(_account, address(0), _value);
     }
